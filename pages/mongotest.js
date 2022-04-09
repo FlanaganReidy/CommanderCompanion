4df5f231a07c131a0da847bdbde8e6b63965feea
Shannon Reidy <shannonf.reidy@gmail.com>
2022-04-09 13:21:45 -0500

    reading from mongo db

--- pages/mongotest.js
+++ pages/mongotest.js
@@ -1,24 +1,43 @@
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 import clientPromise from '../lib/mongodb'
+//import { db } from 'mongodb'
 
-export default function MongoTest({isConnected}){
+export default function MongoTest({isConnected, movies}){
     return(
         <div className={styles.container}>
             <h1>This is the Mongo Test</h1>
             {isConnected ? (<h3>You are connected to MongoDB</h3>):(<h3>You are not connected to MongoDB</h3>)}
-        <Link href="/">
-            <a className={styles.card}>Back to home</a>
-        </Link>
+            <ul>
+                {movies.map((movie)=>(
+                <li>
+                    <h2>{movie.title}</h2>
+                    <h3>{movie.metacritic}</h3>
+                    <p>{movie.plot}</p>
+                </li>    
+                ))}
+            </ul>
         </div>
         )
 }
 
-export async function getServerSideProps(context){
-    try {
-        await clientPromise
+export async function getServerSideProps(){
+    try { 
+        const client = await clientPromise
+
+        const movies = await client.db("sample_mflix")
+        .collection("movies")
+        .find({})
+        .sort({metacritic: -1})
+        .limit(20)
+        .toArray();
+
         return {
-            props:{isConnected:true},
+            props:{
+                isConnected:true,
+                movies: JSON.parse(JSON.stringify(movies)),
+            },
+            
         }
     } catch (e){
         console.error(e)
