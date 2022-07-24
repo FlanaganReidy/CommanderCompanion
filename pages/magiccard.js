813961e409a4a2fbee4e2680b87d327e2582933a
Shannon Reidy <shannonf.reidy@gmail.com>
2022-07-23 19:36:13 -0500

    additional construction on card page. Styling added

--- pages/magiccard.js
+++ pages/magiccard.js
@@ -1,19 +1,90 @@
 
 import Image from "next/image";
+import dynamic from 'next/dynamic';
+import {Suspense} from 'react';
 import carddata from "../public/samplecard";
+import {Row,Col,Container,Accordion} from "react-bootstrap"
+
 
 export default function magiccard(props){
     console.log(props)
     return(
-        props.mycard.map((card) => (
+        <Container fluid>
+            {props.mycard.map((card) => (
         <div>
             <h2>{card.name}</h2>
             <div>
-            <Image src={card.image_uris.normal} layout="responsive" width={100} height={120}></Image>
-            <p>{card.color_identity}</p>
+                <Row>
+                    <Col s={12} md={6} lg={4}>
+                    <img className = "cardImage" src={card.image_uris.normal}></img>
+                    </Col>
+
+                    <Col md={6} lg={4}>
+                    <Row>
+                        <Col>
+                            USD
+                        </Col>
+                        <Col>
+                            USD Foil
+                        </Col>
+                        <Col>
+                            USD Etched
+                        </Col>
+                        <Col>
+                            EUR
+                        </Col>
+                        <Col>
+                            EUR Foil
+                        </Col>
+                        <Col>
+                            Tix
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            {card.prices.usd}
+                        </Col>
+                        <Col>
+                            {card.prices.usd_foil}
+                        </Col>
+                        <Col>
+                            {card.prices.usd_etched}
+                        </Col>
+                        <Col>
+                            {card.prices.eur}
+                        </Col>
+                        <Col>
+                            {card.prices.eur_foil}
+                        </Col>
+                        <Col>
+                            {card.prices.tix}
+                        </Col>
+                    </Row>
+                    <Row>
+                    <Accordion defaultActiveKey="0">
+                        <Accordion.Item eventKey="0" >
+                            <Accordion.Header>Oracle Text</Accordion.Header>
+                            <Accordion.Body>
+                            {card.oracle_text}
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="1">
+                            <Accordion.Header>Flavor Text</Accordion.Header>
+                            <Accordion.Body>
+                            {card.flavor_text}
+                            </Accordion.Body>
+                         </Accordion.Item>
+                    </Accordion>
+                    </Row>              
+                    </Col>
+                </Row>
             </div>
         </div>))
+        }
+    </Container>
     )
+    
+    
 }
 export const getServerSideProps = () => ({
     props:{
